81035025e11deefcabc2620763a12ac7a7c9e0d7
Letícia dos Santos Alves <leticia.s.a.tecns@gmail.com>
2025-05-23 00:21:48 -0300

    Delete travel-diary directory

--- travel-diary/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// Seu IP local Wi-Fi
-const api = axios.create({
-  baseURL: 'http://192.168.56.1:8080', // backend rodando na porta 8080
-});
-
-export default api;
--- travel-diary/api/viagens.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export const criarDiarioViagem = async (viagem) => {
-  const response = await api.post('/diarioviagens', viagem);
-  return response.data;
-};
